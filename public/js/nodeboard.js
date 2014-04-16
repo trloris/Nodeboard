fa5bdc02b2d2d7b2a5301c29f82d1ec184fbf2bf
Trevyn Loris <trevyn.loris@gmail.com>
2014-04-16 14:23:05 -0500

    Remove token from JS

--- public/js/nodeboard.js
+++ public/js/nodeboard.js
@@ -1,18 +1,16 @@
-var getReplies = function(topicID, replyID, token) {
+var getReplies = function(topicID, replyID) {
     var req = new XMLHttpRequest();
     req.open("POST", "/newreplies/topic/" + topicID + "/reply/" + replyID);
     req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     req.onload = function() {
         var response = JSON.parse(req.response);
         if (typeof response.length === 'undefined') {
-            getReplies(topicID, replyID, token);
+            getReplies(topicID, replyID);
         } else {
             var lastReply = response[response.length - 1].id;
             var messageHeader = document.createElement('div');
-            messageHeader.innerHTML = 'Fuck';
-
-            getReplies(topicID, lastReply, token);
+            getReplies(topicID, lastReply);
         }
     }
-    req.send("_csrf=" + token);
+    req.send();
 }
